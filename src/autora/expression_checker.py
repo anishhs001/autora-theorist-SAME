0b870ab924c60e967d1bb5117c12b146a29f7e86
anishhs001 <108971115+anishhs001@users.noreply.github.com>
2024-09-30 23:46:23 +0200

    Update expression_checker.py

--- src/autora/expression_checker.py
+++ src/autora/expression_checker.py
@@ -4,17 +4,43 @@ import numpy as np
 
 
 class ExpressionChecker:
+"""
+A class to evaluate and generate expressions from a DataFrame, 
+and compute correlations between the generated features and a target variable.
 
+Parameters:
+-----------
+df : pd.DataFrame
+The input DataFrame containing feature data.
+y : pd.Series or pd.DataFrame
+The target variable(s) for which correlations with generated features are calculated.
+"""
     def __init__(self, df, y):
+        """
+        Initializes the ExpressionChecker by sampling 75 rows if the DataFrame has more than 75 rows.
     
-    if len(df)>=75 and len(y)>=75:
-        self.df = df.sample(n = 75, random_state = 42)
-        self.y = y.sample(n = 75, random_state = 42)
-    else:
-        self.df = df
-        self.y = y
+        Parameters:
+        -----------
+        df : pd.DataFrame
+            The DataFrame to sample from and use in expression generation.
+        y : pd.Series
+            The target variable(s) associated with the DataFrame.
+        """
+        if len(df)>=75 and len(y)>=75:
+            self.df = df.sample(n = 75, random_state = 42)
+            self.y = y.sample(n = 75, random_state = 42)
+        else:
+            self.df = df
+            self.y = y
     def output(self):
-        
+        """
+        Generates new expressions based on the input DataFrame and calculates the correlation
+        between these new features and the target variable.
+        Returns:
+        --------
+        pd.DataFrame:
+            A DataFrame containing the feature with the highest absolute correlation to the target variable.
+        """
         expression_generator = ExpressionGenerator(self.df.columns)
         evaluated_columns = expression_generator.dataframe_from_expr(self.df)
         # Create a new DataFrame from the evaluated columns
@@ -33,4 +59,5 @@ class ExpressionChecker:
         greatest_val = correlation_df.iloc[0]['Correlation']
         feature_name = correlation_df.iloc[0]['Feature']
         return result_df[[feature_name]]
+
 
